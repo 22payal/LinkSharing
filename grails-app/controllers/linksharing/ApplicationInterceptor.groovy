7f14eb4d14a155f0170beb38819aabe4df2e4b63
22payal <payalnigam1996@gmail.com>
2018-03-30 01:42:46 +0530

    question11

--- grails-app/controllers/linksharing/ApplicationInterceptor.groovy
+++ grails-app/controllers/linksharing/ApplicationInterceptor.groovy
@@ -2,4 +2,20 @@ package linksharing
 
 
 class ApplicationInterceptor {
+
+    ApplicationInterceptor()
+    {
+        matchAll()
+    }
+    boolean before() {   log.info("ACTION AND CONTROLLER LOG: ${params.toString()}")
+        true
+    }
+
+    boolean after() {  log.info("ACTION AND CONTROLLER LOG: ${params.toString()}")
+        true }
+
+    void afterView() {
+        // no-op
+    }
+
 }
