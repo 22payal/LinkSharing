5a7efc8e8e3026955d6b7fb0da5a2e0fc940f95c
22payal <payalnigam1996@gmail.com>
2018-03-30 01:28:01 +0530

    question6

--- grails-app/controllers/linksharing/LoginController.groovy
+++ grails-app/controllers/linksharing/LoginController.groovy
@@ -4,37 +4,30 @@ package linksharing
 class LoginController {
 
     def index() {
-        if(session.user)
-            forward(controller: 'user',action:'index')
+        if (session.user)
+            forward(controller: 'user', action: 'index')
         else
-            render( 'failure')
+            render('failure')
 
     }
+
     def logout() {
         session.invalidate()
-        redirect(action:'index')
-
- }
-
-    def loginHandler(String userName,String password)
-    {
-        User user = User.findByUserNameAndPassword(userName, password)
-        if(user!=null) {
-            if(user.active) {
-                session.user=user
-                forward(controller: 'user', action: 'index')
-
-            }
-            else {
-                flash.error = "Your account is not active"
-
-            }
-        }
-        else
-        {
-            flash.error="User not found"
-        }
         redirect(action: 'index')
-    }
 
     }
+
+    def loginHandler(String userName, String password) {
+        User user = User.findByUserNameAndPassword(userName, password)
+        if (user != null) {
+            if (user.active) {
+                session.user = user
+
+            }
+
+            redirect(action: 'index')
+
+
+        }
+    }
+}
