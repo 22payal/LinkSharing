d07dc25348248031c8f05bb7b87fa82dd3ecfb49
22payal <payalnigam1996@gmail.com>
2018-03-30 11:24:58 +0530

    question7

--- src/main/groovy/enumeration/Visibility.groovy
+++ src/main/groovy/enumeration/Visibility.groovy
@@ -1,6 +1,30 @@
 package enumeration
 
 enum Visibility {
-    PRIVATE ,PUBLIC
+    PRIVATE (0),PUBLIC(1)
 
-}
+    Visibility (int value)
+    {
+        this.value=value
+    }
+
+     private final int value
+
+     int getValue() {
+        value
+    }
+    }
+
+ class Demo
+        {
+            public static void main(String[] args) {
+                convert("PUBLIC")
+            }
+
+             static int convert(String inputString) {
+                    Visibility visibility = inputString
+                    //println visibility
+                    return visibility.value
+                }
+
+        }
