cc6ab1237ba2b773aaa1b7884467c0ec6a4c3025
22payal <payalnigam1996@gmail.com>
2018-03-30 13:07:59 +0530

    question4

--- grails-app/controllers/linksharing/ResourceController.groovy
+++ grails-app/controllers/linksharing/ResourceController.groovy
@@ -7,5 +7,12 @@ class ResourceController {
     def resourceDelete(Integer id)
     {
         Resource resource=Resource.load(id)
+
+        if(!resource)
+        {
+            render("exception of object not found")
+        }
     }
+
+
 }
